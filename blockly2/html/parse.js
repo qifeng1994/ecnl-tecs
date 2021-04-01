fbda8956980dc786a9571e62bbdf5b80ec9ccbf6
qifeng1994 <maidoufengfeng@gmail.com>
2021-04-01 15:10:46 +0900

    devices中有四组value为oneOf的设备：0260,0263,027E,0288

--- blockly2/html/parse.js
+++ blockly2/html/parse.js
@@ -1,5 +1,18 @@
 // JSON.parse(text[,reviver])
+// JSON 语法是 JavaScript 对象表示语法的子集
+// 数据在"key":"value"对中; 数据由逗号分隔
+// 大括号 {} 保存对象; 对象可以包含多个"key":"value"
+// 中括号 [] 保存数组，数组可以包含多个对象{}
+// 访问对象的value通过 obj.key 或 obj["key"]
 
+// ECNL.json:
+// {
+//     "metaData":{key:value, key:value},
+//     "definitions":{key:value, "state_ON-OFF-3031":value, key:value},
+//     "devices":{"0x0EF0":{value}, "0x0260":{"oneOf":[{key:value},{key:value}] } }
+// }
+
+// "0x0260" "0x0263" "0x027E" "0x0288" : {"oneOf":[value]}
 window.onload = function (){
     var url = "../../json_parse/appendix_v3-1-6r5/EL_DeviceDescription_3_1_6r5.json"
     var request = new XMLHttpRequest();
@@ -10,13 +23,12 @@ window.onload = function (){
         var devices = json.devices
         // 遍历对象
         for(let deviceId in devices){ //deviceId: 0x0EF0
-            // var deviceContent = devices[deviceId]
-            console.log(devices["0x0EF0"]);
-            // console.log(deviceContent);
-            // if (deviceContent == 'oneOf') {
-            //     console.log(true);
-            // }
-            // else console.log(false);
+            var deviceContent = devices[deviceId]
+            for (let x in deviceContent){
+                if (x == 'oneOf'){
+                    console.log(deviceId);
+                }
+            }
         }
         
     }
