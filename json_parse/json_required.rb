eeb7edd00e465d382fcdc4ae58f1df78122bbd22
qifeng1994 <maidoufengfeng@gmail.com>
2021-04-13 23:16:10 +0900

    update

--- json_parse/json_required.rb
+++ json_parse/json_required.rb
@@ -13,14 +13,19 @@ Definitions = DevDesc["definitions"]
 def parse_properties (val,className)
     val.each{ |prop_id,val2|
         if val2['oneOf'] then
-
+            
         else
             if val2['accessRule']['set'] == 'required' then
-                # print "#{val2['propertyName']['en']}\n"
                 functionName = val2['propertyName']['en']
+                if val2['data']['$ref'] then
                 ref = val2['data']['$ref'].sub( /#\/definitions\//, "" )
                 val3 = Definitions[ref]
                 parse_definitions(val3,functionName,className)
+                elsif val2['data']['type'] == 'state' then
+                val3 = val2['data']
+                parse_definitions(val3,functionName,className)
+                else
+                end
             else
             end
         end
@@ -32,9 +37,8 @@ def parse_definitions(val,functionName,className)
         if val['type'] == 'state' then
             size = val['size']
             type = property_data_type(size)
-            name = functionName.split(/ |\_|\-/).map(&:capitalize).join(" ").gsub(/\s+/, '')
+            name = font_change(functionName)
             print_function_state("  ",size,type,name,val,className)
-            # print_function_state
         else
             # print_function
         end
@@ -69,18 +73,23 @@ def print_state_type (indent,val,className)
     }
 end
 
+def font_change(name)
+    return name.split(/ |\_|\-/).map(&:capitalize).join(" ").gsub(/\s+/, '')
+end
 Devices.each{ |id, val|
     if val['oneOf'] then
         # print( "#{id} oneOf\n" )
         val['oneOf'].each{|val2|
             # print( "  #{val2['className']['en']}\n" )
-            className = val2['className']['en'].split(/ |\_|\-/).map(&:capitalize).join(" ").gsub(/\s+/, '')
+            className = font_change(val2['className']['en'])
             parse_properties(val2['elProperties'],className)
 
         }
     elsif val['className'] == nil then
         #print "*** #{id} has no class name ***\n"
     elsif val['className']['en'] then
+        className = font_change(val['className']['en'])
+        parse_properties(val['elProperties'],className)
         #print( "#{val['className']['en']} = #{id}\n" )
         #print_properties( val['elProperties'] )
     elsif val['className']['ja'] then
