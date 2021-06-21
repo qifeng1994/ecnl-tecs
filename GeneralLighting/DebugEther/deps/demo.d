bbdfcd35acfd315cd452aec470b0e59b61e075b1
qifeng1994 <maidoufengfeng@gmail.com>
2021-06-21 11:54:06 +0900

    删除了注释

---
+++ GeneralLighting/DebugEther/deps/demo.d
@@ -0,0 +1,128 @@
+demo.o: ../src/demo.c gen/tECNLGeneralLighting_tecsgen.h \
+ gen/global_tecsgen.h ../../asp3_dcre/tecs_kernel/tecs_kernel.h \
+ ../../asp3_dcre/include/kernel.h ../../asp3_dcre/include/t_stddef.h \
+ ../../asp3_dcre/target/gr_peach_gcc/target_stddef.h \
+ ../../musl-1.1.18/include/stdint.h \
+ ../../musl-1.1.18/include/bits/alltypes.h \
+ ../../musl-1.1.18/include/bits/stdint.h \
+ ../../asp3_dcre/arch/gcc/tool_stddef.h \
+ ../../musl-1.1.18/include/stddef.h ../../musl-1.1.18/include/limits.h \
+ ../../musl-1.1.18/include/features.h \
+ ../../musl-1.1.18/include/bits/limits.h \
+ ../../asp3_dcre/arch/arm_gcc/rza1/chip_stddef.h \
+ ../../asp3_dcre/arch/arm_gcc/common/core_stddef.h \
+ ../../asp3_dcre/target/gr_peach_gcc/target_kernel.h \
+ ../../asp3_dcre/arch/arm_gcc/rza1/chip_kernel.h \
+ ../../asp3_dcre/arch/arm_gcc/common/core_kernel.h \
+ ../../asp3_dcre/syssvc/serial.h ../../asp3_dcre/include/t_syslog.h \
+ ../../asp3_dcre/target/gr_peach_gcc/target_syssvc.h \
+ ../../asp3_dcre/target/gr_peach_gcc/gr_peach.h \
+ ../../asp3_dcre/arch/arm_gcc/common/core_syssvc.h \
+ ../../asp3_dcre/arch/arm_gcc/common/arm.h \
+ ../../asp3_dcre/arch/arm_gcc/common/arm_insn.h \
+ ../../musl-1.1.18/include/termios.h \
+ ../../musl-1.1.18/include/bits/termios.h gen/sTaskBody_tecsgen.h \
+ gen/sGeneralLighting_tecsgen.h gen/tGeneralLighting_tecsgen.h \
+ kernel_cfg.h ../src/echonet_main.h ../../ntshell/echonet/echonet.h \
+ ../../ntshell/echonet/echonet_rename.h \
+ ../../ntshell/echonet/echonet_class.h \
+ ../../ntshell/src/echonet_app_config.h echonet_cfg.h \
+ ../../asp3_dcre/target/gr_peach_gcc/target_kernel_impl.h \
+ ../../asp3_dcre/arch/arm_gcc/rza1/chip_kernel_impl.h \
+ ../../asp3_dcre/arch/arm_gcc/rza1/rza1.h ../../asp3_dcre/include/sil.h \
+ ../../asp3_dcre/target/gr_peach_gcc/target_sil.h \
+ ../../asp3_dcre/arch/arm_gcc/common/core_sil.h \
+ ../../asp3_dcre/arch/arm_gcc/common/gic_kernel_impl.h \
+ ../../asp3_dcre/arch/arm_gcc/common/core_kernel_impl.h
+
+gen/tECNLGeneralLighting_tecsgen.h:
+
+gen/global_tecsgen.h:
+
+../../asp3_dcre/tecs_kernel/tecs_kernel.h:
+
+../../asp3_dcre/include/kernel.h:
+
+../../asp3_dcre/include/t_stddef.h:
+
+../../asp3_dcre/target/gr_peach_gcc/target_stddef.h:
+
+../../musl-1.1.18/include/stdint.h:
+
+../../musl-1.1.18/include/bits/alltypes.h:
+
+../../musl-1.1.18/include/bits/stdint.h:
+
+../../asp3_dcre/arch/gcc/tool_stddef.h:
+
+../../musl-1.1.18/include/stddef.h:
+
+../../musl-1.1.18/include/limits.h:
+
+../../musl-1.1.18/include/features.h:
+
+../../musl-1.1.18/include/bits/limits.h:
+
+../../asp3_dcre/arch/arm_gcc/rza1/chip_stddef.h:
+
+../../asp3_dcre/arch/arm_gcc/common/core_stddef.h:
+
+../../asp3_dcre/target/gr_peach_gcc/target_kernel.h:
+
+../../asp3_dcre/arch/arm_gcc/rza1/chip_kernel.h:
+
+../../asp3_dcre/arch/arm_gcc/common/core_kernel.h:
+
+../../asp3_dcre/syssvc/serial.h:
+
+../../asp3_dcre/include/t_syslog.h:
+
+../../asp3_dcre/target/gr_peach_gcc/target_syssvc.h:
+
+../../asp3_dcre/target/gr_peach_gcc/gr_peach.h:
+
+../../asp3_dcre/arch/arm_gcc/common/core_syssvc.h:
+
+../../asp3_dcre/arch/arm_gcc/common/arm.h:
+
+../../asp3_dcre/arch/arm_gcc/common/arm_insn.h:
+
+../../musl-1.1.18/include/termios.h:
+
+../../musl-1.1.18/include/bits/termios.h:
+
+gen/sTaskBody_tecsgen.h:
+
+gen/sGeneralLighting_tecsgen.h:
+
+gen/tGeneralLighting_tecsgen.h:
+
+kernel_cfg.h:
+
+../src/echonet_main.h:
+
+../../ntshell/echonet/echonet.h:
+
+../../ntshell/echonet/echonet_rename.h:
+
+../../ntshell/echonet/echonet_class.h:
+
+../../ntshell/src/echonet_app_config.h:
+
+echonet_cfg.h:
+
+../../asp3_dcre/target/gr_peach_gcc/target_kernel_impl.h:
+
+../../asp3_dcre/arch/arm_gcc/rza1/chip_kernel_impl.h:
+
+../../asp3_dcre/arch/arm_gcc/rza1/rza1.h:
+
+../../asp3_dcre/include/sil.h:
+
+../../asp3_dcre/target/gr_peach_gcc/target_sil.h:
+
+../../asp3_dcre/arch/arm_gcc/common/core_sil.h:
+
+../../asp3_dcre/arch/arm_gcc/common/gic_kernel_impl.h:
+
+../../asp3_dcre/arch/arm_gcc/common/core_kernel_impl.h:
